6f18a564fbba57bfb96183cf43091dddff2b7d47
art <gulturyan.a@gmail.com>
2022-09-30 17:11:18 +0300

    ajax, request

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ inputUah.addEventListener('input', () => {
     request.send();
 
     request.addEventListener('load', () => {
-        if (request.status === 200) {
+        if (request.readyState === 4 && request.status === 200) {
             console.log(request.response);
             const data = JSON.parse(request.response);
             inputUsd.value = (+inputUah.value / data.current.usd).toFixed(2);
