e6e04518f1c016218677c315563fd8338bfcc9c0
art <gulturyan.a@gmail.com>
2022-09-30 17:05:19 +0300

    ajax, request

--- js/script.js
+++ js/script.js
@@ -1,11 +1,29 @@
 'use strict';
 
-const inputRub = document.querySelector('#rub'),
+const inputUah = document.querySelector('#uah'),
       inputUsd = document.querySelector('#usd');
 
-inputRub.addEventListener('input', () => {
+inputUah.addEventListener('input', () => {
     const request = new XMLHttpRequest();
 
-    request.open(method, url, asunc, );
+    // request.open(method, url, async, login, pass);
+    request.open('GET', './js/current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
+
+    request.addEventListener('load', () => {
+        if (request.status === 200) {
+            console.log(request.response);
+            const data = JSON.parse(request.response);
+            inputUsd.value = (+inputUah.value / data.current.usd).toFixed(2);
+        } else {
+            inputUsd.value = "чтото пошло не так :(";
+        }
+    });
+
+    // status
+    // statusText
+    // response
+    // readyState
 
 });
